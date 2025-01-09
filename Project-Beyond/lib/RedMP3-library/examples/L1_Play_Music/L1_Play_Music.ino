6e80c45245cd2994c4c177a702d56101b5f1c17a
Jaspr3m <jasprem.singh@hva.nl>
2025-01-09 10:28:03 +0100

    initial commit; code that dims the light with a push of a button

---
+++ Project-Beyond/lib/RedMP3-library/examples/L1_Play_Music/L1_Play_Music.ino
@@ -0,0 +1,59 @@
+/*
+/--------basic operations---------------/
+mp3.play();
+mp3.pause();
+mp3.nextSong();
+mp3.previousSong();
+mp3.volumeUp();
+mp3.volumeDown();
+mp3.forward();    //fast forward
+mp3.rewind();     //fast rewind
+mp3.stopPlay();  
+mp3.stopInject(); //when you inject a song, this operation can stop it and come back to the song befor you inject
+mp3.singleCycle();//it can be set to cycle play the currently playing song 
+mp3.allCycle();   //to cycle play all the songs in the TF card
+/--------------------------------/
+
+mp3.playWithIndex(int8_t index);//play the song according to the physical index of song in the TF card
+
+mp3.injectWithIndex(int8_t index);//inject a song according to the physical index of song in the TF card when it is playing song.
+
+mp3.setVolume(int8_t vol);//vol is 0~0x1e, 30 adjustable level
+
+mp3.playWithFileName(int8_t directory, int8_t file);//play a song according to the folder name and prefix of its file name
+                                                            //foler name must be 01 02 03...09 10...99
+                                                            //prefix of file name must be 001...009 010...099
+
+mp3.playWithVolume(int8_t index, int8_t volume);//play the song according to the physical index of song in the TF card and the volume set
+
+mp3.cyclePlay(int16_t index);//single cycle play a song according to the physical index of song in the TF
+
+mp3.playCombine(int16_t folderAndIndex[], int8_t number);//play combination of the songs with its folder name and physical index
+      //folderAndIndex: high 8bit is folder name(01 02 ...09 10 11...99) , low 8bit is index of the song
+      //number is how many songs you want to play combination
+
+About SoftwareSerial library:
+The library has the following known limitations:
+If using multiple software serial ports, only one can receive data at a time.
+
+************************************************** **************************************************/
+#include <SoftwareSerial.h>
+#include "RedMP3.h"
+
+#define MP3_RX 4 //RX of Serial MP3 module connect to D4 of Arduino
+#define MP3_TX 5 //TX to D2, note that D2 can not be used as RX on Mega2560, you should modify this if you do not use Arduino UNO
+MP3 mp3(MP3_RX, MP3_TX);
+
+int8_t index  = 2; // 1 - 10
+int8_t volume = 15; // 0 - 30 
+void setup()
+{
+  delay(500);//Requires 500ms to wait for the MP3 module to initialize  
+  mp3.playWithVolume(index, volume);
+  delay(50);//you should wait for >=50ms between two commands
+}
+
+void loop()
+{
+}
+
--- Project-Beyond/src/main.cpp
+++ Project-Beyond/src/main.cpp
@@ -1,18 +1,94 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <RedMP3.h>
+#include <ChainableLED.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// D2 = on/off
+// D3 = pressure
+// D8 = music player
+// A1 = knob
+
+#define ON_OFF 2
+#define PRESSURE 3
+#define LIGHT 4
+#define KNOB 1
+#define MP3_RX 8 
+#define MP3_TX 9 
+
+// MP3 player declarations
+MP3 mp3(MP3_RX, MP3_TX);        // MP3 Player
+int8_t index  = 3; // 1 - 10
+int8_t volume = 15; // 0 - 30
+
+// ChainableLED declarations
+ChainableLED leds(5, 6, 1);     // 1 LED on D5, D6
+
+// Variables to keep track of the button state and LED state
+bool previousButtonState = LOW;
+bool ledState = LOW;
+bool dimming = false;
+int brightness = 255; // Initial brightness
+
+// put (function) declarations here:
+// int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // pin modes
+  pinMode(ON_OFF, INPUT);         // Button as ON/OFF input
+  pinMode(LED_BUILTIN, OUTPUT);   // Built-in LED as output
+  pinMode(KNOB, INPUT);           // Knob 
+
+  // start serial monitor
+  Serial.begin(9600);             // Write to Serial Monitor
+
+  // music starts playing 
+  delay(500);
+  mp3.playWithVolume(index, volume);
+  delay(50);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // ON/OFF BUTTON STUFF
+  bool buttonState = digitalRead(ON_OFF);
+  bool pressed = true;  
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Check if the button is pressed
+  if (buttonState == pressed) {
+    // Start the dimming process
+    dimming = true;
+  } else {
+    // Stop the dimming process
+    dimming = false;
+    brightness = 255; // Reset brightness
+    digitalWrite(LED_BUILTIN, LOW); // Ensure the built-in LED is off
+    leds.setColorRGB(0, 0, 0, 0); // Ensure the Chainable LED is off
+  }
+
+  // Dimming process
+  if (dimming) {
+    if (brightness > 0) {
+      brightness -= 5; // Decrease brightness
+      analogWrite(LED_BUILTIN, brightness); // Adjust the built-in LED brightness
+      leds.setColorRGB(0, brightness, 0, 0); // Adjust the Chainable LED brightness
+      delay(50); // Delay to make the dimming visible
+    } else {
+      dimming = false; // Stop dimming when brightness reaches 0
+      digitalWrite(LED_BUILTIN, LOW); // Ensure the built-in LED is off
+      leds.setColorRGB(0, 0, 0, 0); // Ensure the Chainable LED is off
+    }
+  }
+
+  // ----------------- KNOB STUFF -----------------
+
+  // int state = analogRead(KNOB);
+  
+  // if (state > 511)
+  // {
+  //   Serial.print("Past the threshold: ");
+  //   Serial.println(state);
+  // } else {
+  //   Serial.print("Below the threshold: ");
+  //   Serial.println(state);
+  // }
+  // delay(500);
 }
